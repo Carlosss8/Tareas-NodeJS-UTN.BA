414629f2c2526cf6dccfda56986177758743a3d4
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-04-05 15:28:56 -0300

    Point 2, import of the "OS" module

--- tarea-1/app.js
+++ tarea-1/app.js
@@ -1,2 +1,7 @@
 console.log("Hola me llamo Carlos Rodriguez")
 console.log("Quiero seguir mejorando mis stack como developer y poder crecer profesionalmente")
+
+const os = require('os')
+
+console.log("Sistema operativo: ", os.platform())
+console.log("Memoria libre: ", os.freemem() / 1024 / 1024 / 1024 / 1024)

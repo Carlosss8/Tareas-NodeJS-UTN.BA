0eef331f463e5eedbed1d937e27f65be6566b802
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-04-23 20:44:59 -0300

    Update new schema

--- tarea-3/server.js
+++ tarea-3/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
 const port = 3000;
@@ -9,14 +9,25 @@ app.use(express.json());
 const connectMongoDb = async () => {
     try {
         await mongoose.connect("mongodb://localhost:27017/db_tarea_3")
+        console.log('✅ Conexión a MongoDB establecida');
+
     } catch (error) {
         console.log(error.message)
+        console.error('❌ Error al conectar a MongoDB:', error);
     }
 }
 
-const clotheSchema = new
+const clotheSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: false }
+}, {
+    versionKey: false
+})
 
-    app.listen(port, () => {
-        console.log(`Servidor escuchando en http://localhost:${port}`)
-        connectMongoDb()
-    })
+const Clothes = mongoose.model("Clothes", clotheSchema)
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`)
+    connectMongoDb()
+})

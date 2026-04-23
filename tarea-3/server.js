c767a3b7c90744e4cebd1503b67c2faf93383583
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-04-23 20:11:47 -0300

    Update new connection MondoDB

--- tarea-3/server.js
+++ tarea-3/server.js
@@ -5,3 +5,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const connectMongoDb = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/db_tarea_3")
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const clotheSchema = new
+
+    app.listen(port, () => {
+        console.log(`Servidor escuchando en http://localhost:${port}`)
+        connectMongoDb()
+    })
